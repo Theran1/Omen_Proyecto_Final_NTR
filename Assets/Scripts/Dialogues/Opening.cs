6f63008811ca09e3bbf4614cb8799b9bcd908b34
Theran1 <addg98@gmail.com>
2023-03-30 19:18:46 +0200

    Dialogue system Beginning
    Co-Authored-By: Weedzard <59049803+TitoLuce@users.noreply.github.com>

---
+++ Assets/Scripts/Dialogues/Opening.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class Opening : MonoBehaviour
+{
+    public NPCConversation Conversation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConversationManager.Instance.StartConversation(Conversation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
