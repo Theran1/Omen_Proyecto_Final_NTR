f6118a33ecad762c37c1ecf70c5adcc0616a252a
WittIsHere <59049883+WittIsHere@users.noreply.github.com>
2023-06-06 23:16:21 +0200

    Updating debug more

--- Assets/Scripts/Interaction System/Interactables/NPC.cs
+++ Assets/Scripts/Interaction System/Interactables/NPC.cs
@@ -11,12 +11,42 @@ public class NPC : MonoBehaviour, Interface_Interactable
     public NPCConversation RecievedDialogue;
 
     //---------------------------------------
-    int reputationLibra;
+
+    float reputationAries;
+    float reputationTaurus;
+    float reputationGemini;
+    float reputationCancer;
+    float reputationLeo;
+    float reputationVirgo;
+    float reputationLibra;
+    float reputationScorp;
+    float reputationSagi;
+    float reputationCapri;
+    float reputationAcua;
+    float reputationPsicis;
+
+    private SysHandler sysHandler;
     private Inventory inventory;
 
     private void Awake()
     {
+        sysHandler = GameObject.Find("Debug").GetComponentInChildren<SysHandler>();
         inventory= GameObject.Find("HumanMale_Character_FREE").GetComponent<Inventory>();
+    }
+
+    private void Start()
+    {
+        reputationAries = sysHandler.repAries;
+        reputationTaurus = sysHandler.repTaurus;
+        reputationGemini = sysHandler.repGemini;
+        reputationCancer = sysHandler.repCancer;
+        reputationVirgo = sysHandler.repVirgo;
+        reputationLibra = sysHandler.repLibra;
+        reputationScorp = sysHandler.repScorpio;
+        reputationSagi = sysHandler.repSagi;
+        reputationCapri = sysHandler.repCapri;
+        reputationAcua = sysHandler.repAqua;
+        reputationPsicis = sysHandler.repPiscis;
     }
 
     public bool Interact(Interactor interactor)
@@ -36,7 +66,7 @@ public class NPC : MonoBehaviour, Interface_Interactable
             {
                 ConversationManager.Instance.SetBool("HasBread", false);
             }
-            reputationLibra = ConversationManager.Instance.GetInt("reputationLibra");
+           // reputationLibra = ConversationManager.Instance.GetInt("reputationLibra");
 
             /*if
             ConversationManager.Instance.(nombre de la accion seleccionada) = "goodforlibra";
@@ -55,9 +85,24 @@ public class NPC : MonoBehaviour, Interface_Interactable
 
     private void Update()
     {
-
+        UpdateReps();
         //reputationLibra = ConversationManager.Instance.GetInt("reputationLibra");
         //Debug.Log("libraRep: " + reputationLibra);
+    }
+
+    private void UpdateReps()
+    {
+        reputationAries = sysHandler.repAries;
+        reputationTaurus = sysHandler.repTaurus;
+        reputationGemini = sysHandler.repGemini;
+        reputationCancer = sysHandler.repCancer;
+        reputationVirgo = sysHandler.repVirgo;
+        reputationLibra = sysHandler.repLibra;
+        reputationScorp = sysHandler.repScorpio;
+        reputationSagi = sysHandler.repSagi;
+        reputationCapri = sysHandler.repCapri;
+        reputationAcua = sysHandler.repAqua;
+        reputationPsicis = sysHandler.repPiscis;
     }
 
 }
--- Assets/Scripts/Interaction System/SysHandler.cs
+++ Assets/Scripts/Interaction System/SysHandler.cs
@@ -1,27 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class SysHandler : MonoBehaviour
 {
     // We will need 12 ints in order to represent the reputation with each faction
 
+    public Canvas canvas;
 
-    float repTaurus = 0.0f;
-    float repGemini = 0.0f;
-    float repCancer = 0.0f;
-    float repLeo = 0.0f;
-    float repVirgo = 0.0f;
-    float repLibra = 0.0f;
-    float repScorpio = 0.0f;
-    float repSagi = 0.0f;
-    float repCapri = 0.0f;
-    float repAqua = 0.0f;
-
-    float repAries = 0.0f;
-    float repPiscis = 0.0f;
-
+    public float repTaurus = 0.0f;
+    public float repGemini = 0.0f;
+    public float repCancer = 0.0f;
+    public float repLeo = 0.0f;
+    public float repVirgo = 0.0f;
+    public float repLibra = 0.0f;
+    public float repScorpio = 0.0f;
+    public float repSagi = 0.0f;
+    public float repCapri = 0.0f;
+    public float repAqua = 0.0f;
+   
+    public float repAries = 0.0f;
+    public float repPiscis = 0.0f;
+   
     public Slider sliderTaurus;
     public Slider sliderGemini;
     public Slider sliderCancer;
@@ -38,15 +40,32 @@ public class SysHandler : MonoBehaviour
 
 
 
-    void Start()
+    void Awake()
     {
-        repAries = 90.0f;
-        repPiscis = 10.0f;
+       repAries = 90.0f;
+       repPiscis = 10.0f;
+
+       repTaurus = 50.0f;
+       repGemini = 50.0f;
+       repCancer = 50.0f;
+       repLeo = 50.0f;
+       repVirgo = 50.0f;
+       repLibra = 50.0f;
+       repScorpio = 50.0f;
+       repSagi = 50.0f;
+       repCapri = 50.0f;
+       repAqua = 50.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if(Keyboard.current.f2Key.wasPressedThisFrame)
+        {
+            canvas.enabled = !canvas.enabled;
+        }
+
         UpdateSliders();
     }
 
@@ -62,8 +81,57 @@ public class SysHandler : MonoBehaviour
         sliderSagi.value = repSagi;
         sliderCapri.value = repCapri;
         sliderAqua.value = repAqua;
+
+
         sliderAries.value = repAries;
         sliderPiscis.value = repPiscis;
     }
 
+    public void PiscisChange(int num)
+    {
+        repPiscis = repPiscis + num;
+
+        repTaurus = repTaurus + (num/2);
+        repAries = repAries - (num / 2);
+    }
+
+    public void AriesChange(int num)
+    {
+        repAries = repAries + num;
+
+        repGemini = repGemini + (num / 2);
+        repPiscis = repPiscis - (num / 2);
+    }
+
+    public void TaurusChange(int num)
+    {
+        repTaurus = repTaurus + num;
+
+        repPiscis = repPiscis + (num / 2);
+        repLeo = repLeo - (num / 2);
+    }
+
+    public void GeminiChange(int num)
+    {
+        repGemini = repGemini + num;
+
+        repAries = repAries + (num / 2);
+        repCapri = repCapri - (num / 2);
+    }
+
+    public void LibraChange(int num)
+    {
+        repLibra = repLibra + num;
+
+        repLeo = repLeo + (num / 2);
+        repVirgo = repVirgo - (num / 2);
+    }
+
+    public void VirgoChange(int num)
+    {
+        repVirgo = repVirgo + num;
+
+        repCancer = repCancer + (num / 2);
+        repLibra = repLibra - (num / 2);
+    }
 }
